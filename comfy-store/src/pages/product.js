3455740e7159e3a04fea28021c0953a93d2b6c5b
Tornike <tokoeqizashvili2486@gmail.com>
2024-01-20 18:12:56 +0400

    added new project

--- comfy-store/src/pages/product.js
+++ comfy-store/src/pages/product.js
@@ -1,24 +1,65 @@
 // global imports
-import '../toggleSidebar.js';
-import '../cart/toggleCart.js';
-import '../cart/setupCart.js';
+import "../toggleSidebar.js";
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
 // specific
-import { addToCart } from '../cart/setupCart.js';
-import { singleProductUrl, getElement, formatPrice } from '../utils.js';
+import { addToCart } from "../cart/setupCart.js";
+import { singleProductUrl, getElement, formatPrice } from "../utils.js";
 
 // selections
-// const loading = getElement('.page-loading');
-// const centerDOM = getElement('.single-product-center');
-// const pageTitleDOM = getElement('.page-hero-title');
-// const imgDOM = getElement('.single-product-img');
-// const titleDOM = getElement('.single-product-title');
-// const companyDOM = getElement('.single-product-company');
-// const priceDOM = getElement('.single-product-price');
-// const colorsDOM = getElement('.single-product-colors');
-// const descDOM = getElement('.single-product-desc');
-// const cartBtn = getElement('.addToCartBtn');
+const loading = getElement(".page-loading");
+const centerDOM = getElement(".single-product-center");
+const pageTitleDOM = getElement(".page-hero-title");
+const imgDOM = getElement(".single-product-img");
+const titleDOM = getElement(".single-product-title");
+const companyDOM = getElement(".single-product-company");
+const priceDOM = getElement(".single-product-price");
+const colorsDOM = getElement(".single-product-colors");
+const descDOM = getElement(".single-product-desc");
+const cartBtn = getElement(".addToCartBtn");
 
 // cart product
-// let productID;
+let productID;
 
 // show product when page loads
+window.addEventListener("DOMContentLoaded", async function () {
+  const urlID = window.location.search;
+
+  try {
+    const response = await fetch(`${singleProductUrl}${urlID}`);
+    if (response.status >= 200 && response.status <= 299) {
+      const product = await response.json();
+      // grab data
+      const { id, fields } = product;
+      productID = id;
+      const { company, price, name, colors, description } = fields;
+      const image = fields.image[0].thumbnails.large.url;
+      // set value
+      document.title = `${name.toUpperCase()} | Comfy`;
+      pageTitleDOM.textContent = `Home / ${name}`;
+      imgDOM.src = image;
+      titleDOM.textContent = name;
+      companyDOM.textContent = company;
+      priceDOM.textContent = formatPrice(price);
+      descDOM.textContent = description;
+      colorsDOM.innerHTML = colors
+        .map((color) => {
+          return `<span class="product-color" style="background-color: ${color}"></span>`;
+        })
+        .join("");
+    } else {
+      console.log(response.status, response.statusText);
+      centerDOM.innerHTML = `<div>
+      <h3 class="error">sorry, something went wrong</h3>
+      <a href="index.html" class="btn">back home</a>
+      </div>`;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  loading.style.display = "none";
+});
+
+cartBtn.addEventListener("click", () => {
+  addToCart(productID);
+});
